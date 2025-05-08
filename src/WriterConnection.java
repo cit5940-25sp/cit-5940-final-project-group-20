c29c6242a7dc5991bb3ad13ee5459b5f7bee2793
Soojin-Lee0819 <sl7362@nyu.edu>
2025-05-08 13:50:40 -0400

    added writer connection javadoc

--- src/WriterConnection.java
+++ src/WriterConnection.java
@@ -1,4 +1,19 @@
+/**
+ * A connection strategy that determines whether two movies are connected by writer.
+ * <p>
+ * Two movies are considered connected if they share the same writer.
+ */
+
 public class WriterConnection implements ConnectionStrategy {
+    /**
+     * Checks whether two movies are connected based on the writer.
+     * <p>
+     * If either movie does not have a writer (i.e., {@code null}), they are not considered connected.
+     *
+     * @param a the first movie
+     * @param b the second movie
+     * @return {@code true} if both movies have the same writer; {@code false} otherwise
+     */
     @Override
     public boolean areConnected(Movie a, Movie b) {
         String writerA = a.getWriter();
@@ -10,7 +25,11 @@ public class WriterConnection implements ConnectionStrategy {
         }
         return writerA.equals(writerB);
     }
-
+    /**
+     * Returns the type of connection this strategy represents.
+     *
+     * @return the string "Writer"
+     */
     @Override
     public String getType() {
         return "Writer";
