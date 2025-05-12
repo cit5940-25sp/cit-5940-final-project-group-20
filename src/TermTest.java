e6ca43e751a79bf642cfe45de7e30ee227866bfe
Soojin-Lee0819 <sl7362@nyu.edu>
2025-05-12 10:27:43 -0400

    added term test test cases

--- src/TermTest.java
+++ src/TermTest.java
@@ -53,4 +53,38 @@ public class TermTest {
         assertEquals(hopeful, t1.toString());
     }
 
+    @org.junit.Test
+    public void testConstructorWithNullTerm() {
+        Movie movie = new Movie("Title", 2000, Arrays.asList("Actor"), "Director", "Writer", "Cinematographer", "Composer", Arrays.asList("Genre"));
+        try {
+            new Term(null, movie);
+            fail("Wrong illegal argument exception when null = 0");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Illegal argument term = 0 ", e.getMessage());
+        }
+    }
+
+    @org.junit.Test
+    public void testGetTerm() {
+        Movie movie = new Movie("Test", 2001, Arrays.asList("Actor"), "Director", "Writer", "Cinematographer", "Composer", Arrays.asList("Genre"));
+        Term term = new Term("Search", movie);
+        assertEquals("Search", term.getTerm());
+    }
+
+    //adding compare to
+    @org.junit.Test
+    public void testCompareToLessThan() {
+        Movie movie = new Movie("M", 2000, Arrays.asList("A"), "D", "W", "C", "C", Arrays.asList("G"));
+        Term t1 = new Term("abc", movie);
+        Term t2 = new Term("bcd", movie);
+        assertEquals(true, t1.compareTo(t2) < 0);
+    }
+
+    @org.junit.Test
+    public void testCompareToGreaterThan() {
+        Movie movie = new Movie("M", 2000, Arrays.asList("A"), "D", "W", "C", "C", Arrays.asList("G"));
+        Term t1 = new Term("z", movie);
+        Term t2 = new Term("y", movie);
+        assertEquals(true, t1.compareTo(t2) > 0);
+    }
 }
